13c26293a895c6b3b2c36cde2362af031a1e73c7
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-07-20 01:32:10 +0200

    update switching with the player

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,33 @@ const cancel = document.getElementById("cancel");
 const confirm = document.getElementById("confirm");
 const form = document.querySelector("form");
 const error = document.getElementById("error");
+const inputField = document.querySelector("input");
+const startGame = document.getElementById("start-game");
+const thePlay = document.getElementById("the-play");
+const activePlayerName =document.getElementById("active-player-name");
+const gameFieldElements = document.querySelectorAll("#play-box li")
+
+let isPlayer1 = true;
+let playerwon = false;
+let selectedPlayer = 0;
+let activePlayer = 0;
+
+const players=[
+  {
+    name: '',
+    Symbol: 'X',
+  },
+  {
+    name: '',
+    Symbol: 'O',
+  },
+]
+for(const fieldElement of gameFieldElements){
+  fieldElement.addEventListener('click',selectGameField)
+}
 
 editAncher[0].addEventListener("click", onName1);
 editAncher[1].addEventListener("click", onName2);
 cancel.addEventListener("click", offName);
-form.addEventListener('submit', confirmName)
+form.addEventListener("submit", confirmName);
+startGame.addEventListener("click", startGameFunction);
--- scripts/config.js
+++ scripts/config.js
@@ -1,33 +1,58 @@
-let isplayer1 = true;
-let playerwon = false;
-
 function onName1() {
-    allOverLay.style.display = "block";
-  isplayer1 = true;
+  allOverLay.style.display = "block";
+  isPlayer1 = true;
+  selectedPlayer = 1;
 }
 function onName2() {
-    allOverLay.style.display = "block";
-  isplayer1 = false;
+  allOverLay.style.display = "block";
+  isPlayer1 = false;
+  selectedPlayer = 2;
 }
 function offName() {
-    allOverLay.style.display = "none";
-    error.textContent = "";
+  allOverLay.style.display = "none";
+  error.textContent = "";
+  inputField.value = "";
+}
+function confirmName(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const userInput = formData.get("input-name").trim();
+  if (!userInput) {
+    error.textContent = "please enter your name!";
+    error.style.color = "red";
+    error.style.margin = "0px";
+    return;
   }
-  function confirmName(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const userInput = formData.get('input-name').trim();
-    if(!userInput){
-        error.textContent = "please enter your name!"
-        error.style.color = "red";
-        error.style.margin = "0px";
-        return;
-    }
-    allOverLay.style.display = "none";
-    if (isplayer1) {
-      console.log(playerNames[0]);
-      playerNames[0].textContent = userInput;
-    } else {
-      playerNames[1].textContent = userInput;
-    }
+  allOverLay.style.display = "none";
+  if (isPlayer1) {
+    console.log(playerNames[0]);
+    players[0].name = userInput;
+    playerNames[0].textContent = userInput;
+  } else {
+    players[1].name = userInput;
+    playerNames[1].textContent = userInput;
   }
+  inputField.value = "";
+}
+function startGameFunction() {
+  if (!players[0].name || !players[1].name) {
+    alert("please enter valid names for both players!!");
+    return;
+  }
+  thePlay.style.display = "block";
+  activePlayerName.textContent = players[activePlayer].name;
+
+}
+function switchPlayer() {
+  activePlayerName.textContent = players[activePlayer].name;
+  if (activePlayer == 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+}
+function selectGameField(event) {
+  event.target.textContent = players[activePlayer].Symbol;
+  event.target.classList.add("disabled");
+  switchPlayer();
+}
